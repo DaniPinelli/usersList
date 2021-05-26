723ce8226a3527c25289fccd36805609015efcde
DaniPinelli <danielpinelli.fullstack@gmail.com>
2021-05-26 19:24:49 -0300

    Query method working

--- main.js
+++ main.js
@@ -5,13 +5,17 @@ const url = 'https://reqres.in/api/users'
 const arrayUsers = fetch(url)
   .then(response => response.json())
   .then(data => {
-        console.log(data.data);
+     //   console.log(data.data);
         
         for (let i = 0; i < data.data.length; i++) {
 
             let user = "<img src=" + data.data[i].avatar + ">";
-
-            document.querySelector("ul").innerHTML += "<li>" + user + "</li>" 
+            user += "<p>" + data.data[i].first_name + "\
+             " + data.data[i].last_name + "\
+             </p><br/><p>" + data.data[i].email + "\
+             </p><br/><p>Show more..."
+            
+     document.querySelector("div").innerHTML += "<div>" + user + "</div>" 
         }
 
     })      
